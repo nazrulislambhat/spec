099d341e07ae87a717bb41febc42ee35a453a258
Nazrul Islam <nazrulislambhat@gmail.com>
2023-03-05 16:15:53 +0530

    themes individual card

---
+++ web/themes/spec/dist/components/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// Example Behavior
+//
+// (($, Drupal) => {
+//   Drupal.behaviors.changeThis = {
+//     attach(settings, context) {
+//       if (context) {
+//         $(context);
+//       }
+//       if (settings) {
+//         (settings.card as { [key: string]: number }).toString();
+//       }
+//     },
+//   };
+// })(jQuery, Drupal);
+
+//# sourceMappingURL=../maps/main.js.map
