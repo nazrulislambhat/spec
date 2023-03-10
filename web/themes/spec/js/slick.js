ca6dcbcc8ac53a74ff8409d6db7f132e6f3f1bff
Nazrul Islam <nazrulislambhat@gmail.com>
2023-03-10 16:37:18 +0530

    message

---
+++ web/themes/spec/js/slick.js
@@ -0,0 +1,32 @@
+(function ($) {
+  $('.view-slideshow .view-content').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+})(jQuery);
